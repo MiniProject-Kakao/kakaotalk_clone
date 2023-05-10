3380b80df3c818fd8eddb3a1b4bd2519ae904167
Young-woo <rladuddn9955@naver.com>
2023-05-10 18:33:16 +0900

    action수정, 인증번호 남은시간 구현

--- js/random.js
+++ js/random.js
@@ -7,4 +7,34 @@ function numCheck1(){
     
     $("#result").text("인증번호는 "+randomnum+" 입니다.");
 
+
+    let countTime = 0;
+    let intervalCall;
+
+    $.time = function(time){
+        countTime = time;
+        intervalCall = setInterval(alertFunc, 1000);
+    }
+
+    $.closeTime = function(){
+        clearInterval(intervalCall);
+    }
+
+    function alertFunc() {
+        let min = Math.floor(countTime/60);
+        let sec = countTime - (60 * min);
+        if(sec > 9){
+            $('.time').text(min + ':' + sec + '');
+        }else {
+            $('.time').text(min + ':0' + sec + '');
+        }
+        if(countTime <= 0){
+            clearInterval(intervalCall);
+        }
+        countTime--;
+    };
+
+    $.time(180);
+
+
 }
