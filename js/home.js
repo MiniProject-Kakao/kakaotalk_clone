a3d2ddab32b1ff02116e308163472f23aa42d82d
KKangdaa <97223303+KKangdaa@users.noreply.github.com>
2023-05-10 17:34:13 +0900

    즐겨찾기 UI 추가 및 추가기능 작업중

--- js/home.js
+++ js/home.js
@@ -1,6 +1,10 @@
-document.getElementById('user').innerHTML += `<div><img src=${
-  user.icon_url
-} /></div>
+document.getElementById('user').innerHTML += `
+  <div>
+    <img
+      src="${user.icon_url}"
+      onerror="this.onerror=null; this.src='../img/profile.svg';"
+    />
+  </div>
   <div>
     <p>${user.name}</p>
     ${user.value.length != 0 ? `<p>${user.value}</p>` : ''}
@@ -10,24 +14,42 @@ document.getElementById('like').innerHTML += friend
   .map(el =>
     el.like
       ? `<li>
-          <div><img src=${el.icon_url} /></div>
-          <div>
-            <p>${el.name}</p>
-            ${el.value.length != 0 ? `<p>${el.value}</p>` : ''}
-          </div>
-        </li>`
+        <div class="profile_img">
+          <img
+            src="${el.icon_url}"
+            onerror="this.onerror=null; this.src='../img/profile.svg';"
+          />
+        </div>
+        <div class="detail">
+          <p>${el.name}</p>
+          ${el.value.length != 0 ? `<p>${el.value}</p>` : ''}
+        </div>
+        <ul id="bookmark" style="display: none">
+          <li>⭐️</li>
+          <li>💬</li>
+        </ul>
+      </li>`
       : ''
   )
   .join('');
 
 document.getElementById('list').innerHTML += friend
   .map(
-    el => `<li>
-      <div><img src=${el.icon_url} /></div>
-      <div>
+    (el, index) => `<li class=${index + 1}>
+      <div class="profile_img">
+        <img
+          src="${el.icon_url}"
+          onerror="this.onerror=null; this.src='../img/profile.svg';"
+        />
+      </div>
+      <div class="detail">
         <p>${el.name}</p>
         ${el.value.length != 0 ? `<p>${el.value}</p>` : ''}
       </div>
+      <ul id="bookmark" style="display: none">
+        <li>⭐️</li>
+        <li>💬</li>
+      </ul>
     </li>`
   )
   .join('');
@@ -39,23 +61,38 @@ function toggle(id) {
     let like = document.getElementById('like');
     let arrow = document.querySelector('#arrowLike');
 
-    if (like.style.display == 'none') {
+    if (like.style.display === 'none') {
       like.style.display = 'block';
     } else like.style.display = 'none';
 
-    if (arrow.style.transform == 'rotate(0deg)') {
-      arrow.style.transform = `rotate(180deg)`;
-    } else arrow.style.transform = `rotate(0deg)`;
+    if (arrow.style.transform === 'rotate(0deg)') {
+      arrow.style.transform = 'rotate(180deg)';
+    } else arrow.style.transform = 'rotate(0deg)';
   } else {
     let list = document.getElementById('list');
     let arrow = document.querySelector('#arrowList');
 
-    if (list.style.display == 'none') {
+    if (list.style.display === 'none') {
       list.style.display = 'block';
     } else list.style.display = 'none';
 
-    if (arrow.style.transform == 'rotate(0deg)') {
-      arrow.style.transform = `rotate(180deg)`;
-    } else arrow.style.transform = `rotate(0deg)`;
+    if (arrow.style.transform === 'rotate(0deg)') {
+      arrow.style.transform = 'rotate(180deg)';
+    } else arrow.style.transform = 'rotate(0deg)';
+  }
+}
+
+const likeTab = document.querySelector('#like');
+const listTab = document.querySelector('#list');
+const li = document.getElementById('bookmark');
+
+listTab.addEventListener('click', bookmarker);
+
+function bookmarker(e) {
+  console.log(e);
+  if (li.style.display === 'none') {
+    li.style.display = 'flex';
+  } else {
+    li.style.display = 'none';
   }
 }
